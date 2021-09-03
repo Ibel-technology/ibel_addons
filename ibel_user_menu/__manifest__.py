823c9d36e026a68e8f8646e6ca17f6923b1fed41
Abda Wone <abdourahmanewone@gmail.com>
2021-09-03 22:56:30 +0000

    [ADD] : ibel_custom. Modules for customize Ibel.

---
+++ ibel_user_menu/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Customize user menu",
+    "version": "14.0",
+    "author": "Ibel technology",
+    "license": "AGPL-3",
+    "category": "base",
+    "depends": [
+        'mail',
+    ],
+    "data": [
+        'views/customize_user_menu.xml',
+    ],
+    "qweb": [
+        'static/src/xml/base.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
