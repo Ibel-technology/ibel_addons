697efde5db7d5a7a9c7c6c41ae962802f461be3b
Abda Wone <abdourahmanewone@gmail.com>
2022-12-08 21:48:57 +0000

    [ADD] : Ibel_accounting. Additional accounting features for Ibel.

---
+++ ibel_accounting/__manifest__.py
@@ -0,0 +1,22 @@
+
+{
+    'name': 'Ibel accounting',
+    'summary': 'accounting features ',
+    'version': '16.0',
+    'category': 'Maintenance',
+    'author': 'Ibel technology',
+    'website': 'https://ibeltechnology.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+        'account_usability',
+        'account_due_list',
+        'account_fiscal_year',
+        'account_tax_balance',
+
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': True
+}
