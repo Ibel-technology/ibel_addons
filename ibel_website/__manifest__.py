0b167b75523b8da87a84b6ee6f6b65b9359d52f5
Abda Wone <abdourahmanewone@gmail.com>
2023-11-07 14:10:54 +0000

    [ADD] : website_odoo_debranding. This module remove Odoo branding on website.

---
+++ ibel_website/__manifest__.py
@@ -0,0 +1,14 @@
+
+{
+    "name" : "Ibel website",
+    "summary": "Remove Odoo Branding from Website",
+    "version": "17.0",
+    "author": "Ibel technology",
+    "website": "https://ibeltechnology.sn",
+    "license": "LGPL-3",
+    "category": "Website",
+    "depends": ["website"],
+    "data": ["views/templates.xml"],
+    "installable": True,
+    "auto_install": True,
+}
